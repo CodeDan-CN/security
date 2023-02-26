77e9359268de5e49d1c29a255d5c80bb26c49f10
Zhangldc <zhangldc@digitalchina.com>
2023-02-26 15:47:38 +0800

    模块新增---oauth2.0 授权码模式（内存实现）

---
+++ authorization-server-memory/src/main/java/cn/codedan/SecurityAuthorizationApplication.java
@@ -0,0 +1,20 @@
+package cn.codedan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: SecurityAuthorizationApplication
+ * @Description: 授权认证服务器启动类
+ * @Author: CodeDan
+ * @Date: 2023/2/26 15:35
+ * @Version: 1.0.0
+ **/
+@SpringBootApplication
+public class SecurityAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAuthorizationApplication.class, args);
+    }
+
+}
